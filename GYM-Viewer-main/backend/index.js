1026b85ab75f3565644629e12b2c42a59bad2743
Harmanjot98kaur <harmanjot98kaur@gmail.com>
2023-07-17 09:26:10 -0400

    This is my Workout Time Websit

---
+++ GYM-Viewer-main/backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const connectToMongo = require('./db')
+const routes = require('./routes/routes')
+const auth = require('./routes/auth')
+require('dotenv').config()
+const app = express();
+connectToMongo()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use('/workout',routes)
+app.use('/auth',auth)
+app.listen(8080,()=>console.log('Server started at port 8080'))
